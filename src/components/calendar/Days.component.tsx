975c5cda2d266c0f4977436996fb584485e5074a
XCrone <ls.xcrone@gmail.com>
2023-02-10 15:16:35 +0300

    feature: create date hook

--- src/components/calendar/Days.component.tsx
+++ src/components/calendar/Days.component.tsx
@@ -1,9 +1,26 @@
-import style from "./Dates.module.scss";
+import { useState } from "react";
+import { useDate } from "../../hooks/date";
+import { DateItems, Days, MonthCurr, MonthNext, MonthPrev, Moth } from "./Days.style";
 
 interface Props {}
 
 const DaysComponent = () => {
-  return <></>;
+  const { date, nextMont, prevMonth } = useDate();
+
+  return (
+    <Days>
+      <Moth>
+        <MonthPrev onClick={() => prevMonth()} type="button">
+          <i className="bi bi-chevron-left"></i>
+        </MonthPrev>
+        <MonthCurr>{`${date.monthStr} ${date.yearNum}`}</MonthCurr>
+        <MonthNext onClick={() => nextMont()} type="button">
+          <i className="bi bi-chevron-right"></i>
+        </MonthNext>
+      </Moth>
+      <DateItems>date items</DateItems>
+    </Days>
+  );
 };
 
 export default DaysComponent;
