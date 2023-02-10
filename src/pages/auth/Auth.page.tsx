aff1b1280ee726e061a9a6939f977e9fe4e3699a
XCrone <ls.xcrone@gmail.com>
2023-02-10 10:46:49 +0300

    feature: move titles in form

--- src/pages/auth/Auth.page.tsx
+++ src/pages/auth/Auth.page.tsx
@@ -1,19 +1,19 @@
 import { useState } from "react";
+import AuthPreloaderComponent from "../../components/auth/AuthPreloader.component";
 import AuthSignInComponent from "../../components/auth/AuthSignIn.component";
 import AuthSignUpComponent from "../../components/auth/AuthSignUp.component";
-import { AuthSubTitle, AuthTitle, AuthWrapper } from "./Auth.style";
+import { IAuthUser } from "../../store/slices/auth.slice";
+import { AuthWrapper } from "./Auth.style";
 
 const AuthPage = () => {
   const [isSignIn, toggleForm] = useState(true);
 
-  const callbackSubmit = () => {};
+  const callbackSubmit = (user: IAuthUser) => {
+    console.log(user);
+  };
 
   return (
     <AuthWrapper>
-      <AuthTitle>Login to Organize Pro</AuthTitle>
-      <AuthSubTitle>
-        Lorem ipsum dolor sit amet, consetetur sadipscing, lorem ipsum dolorsed diam nonumy amet eirmod.
-      </AuthSubTitle>
       {isSignIn && <AuthSignInComponent toggleForm={() => toggleForm(!isSignIn)} callback={callbackSubmit} />}
       {!isSignIn && <AuthSignUpComponent toggleForm={() => toggleForm(!isSignIn)} callback={callbackSubmit} />}
     </AuthWrapper>
