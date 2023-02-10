8cdb2c2227a8cce62967b7b08f2edf99ad339879
XCrone <ls.xcrone@gmail.com>
2023-02-10 10:48:07 +0300

    feature: create auth methods

--- src/store/slices/auth.slice.ts
+++ src/store/slices/auth.slice.ts
@@ -1,17 +1,93 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, Dispatch } from "@reduxjs/toolkit";
+
+export interface IUserData {}
+
+export interface IAuthUser {
+  email: string;
+  password: string;
+}
 
 interface IStateInitial {
   isAuth: boolean;
+  isPending: boolean;
+  error: {
+    state: boolean;
+    value: string;
+  };
 }
 
+export const authSignIn = createAsyncThunk<
+  IUserData,
+  IAuthUser,
+  { dispatch: Dispatch; rejectValue: string; fullFilled: IUserData }
+>("auth/authSignIn", async function (user, { rejectWithValue, fulfillWithValue }) {
+  console.log(user.email, user.password);
+
+  const asd = (): Promise<void> => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve();
+      }, 2000);
+    });
+  };
+
+  await asd();
+  return fulfillWithValue({});
+});
+
+export const authSignUp = createAsyncThunk<
+  IUserData,
+  IAuthUser,
+  { dispatch: Dispatch; rejectValue: string; fullFilled: IUserData }
+>("auth/authSignUp", async function (user, { rejectWithValue, fulfillWithValue }) {
+  console.log(user.email, user.password);
+
+  const asd = (): Promise<void> => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve();
+      }, 2000);
+    });
+  };
+
+  await asd();
+  return fulfillWithValue({});
+});
+
 const initialStateValue: IStateInitial = {
-  isAuth: false,
+  isAuth: true,
+  isPending: false,
+  error: {
+    state: false,
+    value: "",
+  },
 };
 
 export const Auth = createSlice({
   name: "auth",
   initialState: initialStateValue,
   reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(authSignIn.pending, (state) => {
+        state.isPending = true;
+      })
+      .addCase(authSignIn.fulfilled, (state, action) => {
+        state.isPending = false;
+      })
+      .addCase(authSignIn.rejected, (state, action) => {
+        state.isPending = false;
+      })
+      .addCase(authSignUp.pending, (state) => {
+        state.isPending = true;
+      })
+      .addCase(authSignUp.fulfilled, (state, action) => {
+        state.isPending = false;
+      })
+      .addCase(authSignUp.rejected, (state, action) => {
+        state.isPending = false;
+      });
+  },
 });
 
 // export const {} = Auth.actions;
