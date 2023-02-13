83c9e39a22d4791e8d8be9379c4a8c534858d9b5
XCrone <ls.xcrone@gmail.com>
2023-02-13 17:56:45 +0300

    feature: create days component

--- src/components/calendar/Days.component.tsx
+++ src/components/calendar/Days.component.tsx
@@ -1,24 +1,201 @@
-import { useState } from "react";
+import { memo, useEffect, useMemo, useRef, useState } from "react";
 import { useDate } from "../../hooks/date";
-import { DateItems, Days, MonthCurr, MonthNext, MonthPrev, Moth } from "./Days.style";
+import {
+  DayNum,
+  Days,
+  DaysItem,
+  DaysItems,
+  DayWeeek,
+  EventItem,
+  Events,
+  EventDescription,
+  EventTitle,
+  EventWrapper,
+  MonthCurr,
+  MonthNext,
+  MonthPrev,
+  Moth,
+  SheduleItem,
+  SheduleItems,
+  SheduleLine,
+  SheduleTime,
+  SheduleWrapper,
+} from "./Days.style";
+import { areEqual, FixedSizeList, FixedSizeList as List } from "react-window";
+import { useWindowSize } from "../../hooks/windowResize";
+import { useAppDispatch, useAppSelector } from "../../hooks/redux";
+import { IEvent } from "../../models/calendar.models";
+import { fetchEvents } from "../../store/slices/calendar.slice";
 
-interface Props {}
+interface IDay {
+  dayStr: string;
+  dayNum: number;
+  monthNum: number;
+}
+
+interface IColumn {
+  index: number;
+  style: object;
+  data: IDay[];
+}
+
+export interface IParseEvent {
+  id: number;
+  startTime: number;
+  edntTime: number;
+  background: string;
+  title: string;
+  description: string;
+}
 
 const DaysComponent = () => {
-  const { date, nextMont, prevMonth } = useDate();
+  const dispatch = useAppDispatch();
+
+  const [today] = useState(+new Date().toLocaleString("en-US", { day: "numeric" }));
+
+  const calendar = useAppSelector((state) => state.calendar.events);
+
+  const [daysMonth, SetDaysMonth] = useState<IDay[]>([]);
+  const [currDay, setCurrDay] = useState(0);
+  const [times, SetTimes] = useState<number[]>([]);
+  const [events, SetEvents] = useState<IParseEvent[]>([]);
+
+  const { size } = useWindowSize({ totalHeight: 0, totalWidth: 0 });
+  const { date, month } = useDate();
+
+  const refDays = useRef<FixedSizeList | null>(null);
+
+  const parseEvents = (events: IEvent[]) => {
+    SetEvents([]);
+    events.forEach((event) => {
+      const startTime = String(event.eventStart).split("T")[1].split(".")[0];
+      const endTime = String(event.eventEnd).split("T")[1].split(".")[0];
+      const parseEvent: IParseEvent = {
+        startTime: +startTime.split(":")[0],
+        edntTime: +endTime.split(":")[0],
+        title: event.title,
+        background: event.background,
+        id: event.id,
+        description: event.description,
+      };
+      SetEvents((prev) => [...prev, parseEvent]);
+    });
+  };
+
+  const setDay = (day: number) => {
+    setCurrDay(day);
+    console.log(day);
+
+    if (!!refDays.current) {
+      refDays.current.scrollToItem(day, "smart");
+    }
+
+    try {
+      const dateISO = new Date(+date.yearNum, +date.monthNum - 1, day + 1)
+        .toLocaleString()
+        .split(",")[0]
+        .split(".")
+        .reverse()
+        .join("-");
+      const findEvents = calendar.filter((item) => String(item.eventStart).split("T")[0] === dateISO);
+      parseEvents(findEvents);
+    } catch (e) {
+      SetEvents([]);
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    dispatch(fetchEvents());
+    SetTimes(Array.from(Array(24), (_, idx) => idx));
+  }, []);
+
+  useEffect(() => {
+    setDay(+date.dayNum - 1);
+  }, [calendar]);
+
+  useEffect(() => {
+    const countDays = 33 - new Date(+date.yearNum, +date.monthNum - 1, 33).getDate();
+    const filledArray = Array.from(Array(countDays), (_, idx) => {
+      return {
+        dayStr: new Date(+date.yearNum, +date.monthNum - 1, idx + 1).toLocaleString("en-US", {
+          weekday: "short",
+        }),
+        dayNum: idx,
+        monthNum: +date.monthNum - 1,
+      };
+    });
+    SetDaysMonth(filledArray);
+    // setDay(currDay);
+  }, [date.monthNum]);
+
+  const Column = memo(({ index, style, data }: IColumn) => {
+    return (
+      <div style={style}>
+        <DaysItem
+          style={{ borderColor: today - 1 === data[index].dayNum ? "#ff0000" : "#fff" }}
+          onClick={() => setDay(data[index].dayNum)}
+          currDay={currDay === data[index].dayNum}
+          type="button"
+        >
+          <DayWeeek currDay={currDay === data[index].dayNum}>{data[index].dayStr}</DayWeeek>
+          <DayNum>{data[index].dayNum + 1}</DayNum>
+        </DaysItem>
+        <div style={{ width: "10px" }}></div>
+      </div>
+    );
+  }, areEqual);
 
   return (
     <Days>
       <Moth>
-        <MonthPrev onClick={() => prevMonth()} type="button">
+        <MonthPrev onClick={() => month.prev()} type="button">
           <i className="bi bi-chevron-left"></i>
         </MonthPrev>
         <MonthCurr>{`${date.monthStr} ${date.yearNum}`}</MonthCurr>
-        <MonthNext onClick={() => nextMont()} type="button">
+        <MonthNext onClick={() => month.next()} type="button">
           <i className="bi bi-chevron-right"></i>
         </MonthNext>
       </Moth>
-      <DateItems>date items</DateItems>
+      <DaysItems>
+        <List
+          ref={refDays}
+          overscanCount={2}
+          height={60}
+          itemSize={60}
+          itemCount={daysMonth.length}
+          itemData={daysMonth}
+          layout="horizontal"
+          width={size.windowWidth - 40} // padding-left: 20px + padding-right: 20px
+        >
+          {Column}
+        </List>
+      </DaysItems>
+      <SheduleWrapper>
+        <SheduleItems>
+          {times.map((_, idx) => (
+            <SheduleItem key={idx}>
+              <SheduleTime>{`${idx < 10 ? "0" : ""}${idx} : 00`}</SheduleTime>
+              <SheduleLine />
+            </SheduleItem>
+          ))}
+          <EventWrapper>
+            <Events>
+              {events.map((event, idx) => (
+                <EventItem
+                  key={event.id}
+                  background={event.background}
+                  edntTime={event.edntTime}
+                  startTime={event.startTime}
+                >
+                  <EventTitle>{event.title}</EventTitle>
+                  <EventDescription>{event.description}</EventDescription>
+                </EventItem>
+              ))}
+            </Events>
+          </EventWrapper>
+        </SheduleItems>
+      </SheduleWrapper>
     </Days>
   );
 };
--- src/components/calendar/Days.style.ts
+++ src/components/calendar/Days.style.ts
@@ -1,3 +1,155 @@
+import { color, fonts } from "./../../style/variables.style";
 import styled from "styled-components";
+import { IParseEvent } from "./Days.component";
 
-export const Elem = styled.div``;
+export const Days = styled.div`
+  font-size: 17px;
+  font-family: ${fonts.inter};
+`;
+
+export const Moth = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding-top: 25px;
+  padding-bottom: 25px;
+`;
+
+export const MonthPrev = styled.button`
+  font-size: 15px;
+  line-height: 15px;
+`;
+
+export const MonthCurr = styled.h2`
+  text-transform: capitalize;
+  font-size: 17px;
+  font-weight: 600;
+  line-height: 18px;
+`;
+
+export const MonthNext = styled.button`
+  font-size: 15px;
+  line-height: 15px;
+`;
+
+export const DaysItems = styled.div`
+  padding-bottom: 35px;
+`;
+
+export const DaysItem = styled.button<{ currDay: boolean }>`
+  border: 1px solid;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  row-gap: 5px;
+  width: 50px;
+  height: 100%;
+  background: ${(props) => (props.currDay ? "#654ea3" : "#")};
+  background: ${(props) =>
+    props.currDay ? "linear-gradient(180deg, rgba(101, 78, 163, 1) 0%, rgba(234, 175, 200, 1) 100%)" : "#"};
+`;
+
+export const DayWeeek = styled.h4<{ currDay: boolean }>`
+  font-size: 12px;
+  color: ${(props) => (props.currDay ? "#fff" : "#6A6C7A")};
+  line-height: 16px;
+`;
+
+export const DayNum = styled.div`
+  font-weight: 600;
+  font-size: 15px;
+`;
+
+export const SheduleWrapper = styled.div``;
+
+export const SheduleItems = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  row-gap: 20px;
+  &:last-child {
+    margin-bottom: 30px;
+  }
+`;
+
+export const SheduleItem = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  column-gap: 10px;
+  position: relative;
+  height: 20px;
+`;
+
+export const SheduleTime = styled.div`
+  flex: 0 0 50px;
+  font-size: 13px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 20px;
+`;
+
+export const SheduleLine = styled.div<{}>`
+  flex: 1 1 auto;
+  width: 100%;
+  height: 2px;
+  background-color: #2f3045;
+  position: relative;
+  &:before {
+    content: "";
+    position: absolute;
+    top: 50%;
+    left: 0;
+    transform: translateY(-50%);
+    background-color: #2f3045;
+    height: 10px;
+    width: 10px;
+    border-radius: 50%;
+  }
+`;
+
+export const EventWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 75px;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Events = styled.div`
+  height: 100%;
+  width: 100%;
+  position: relative;
+`;
+
+export const EventItem = styled.div<{ startTime: number; edntTime: number; background: string }>`
+  position: absolute;
+  top: ${(props) => props.startTime * 40 + 10 + 2}px;
+  height: ${(props) => (props.edntTime - props.startTime) * 40 - 4}px;
+  left: 0;
+  right: 0;
+  background-color: ${(props) => props.background};
+  text-align: center;
+  border-radius: 5px;
+  padding: 5px;
+`;
+
+export const EventTitle = styled.h4`
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+  text-transform: capitalize;
+`;
+
+export const EventDescription = styled.h4`
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+  color: #8eb5f2;
+`;
