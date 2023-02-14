96821492aff6b25f8f8afd537fbf2adefa2d2765
XCrone <ls.xcrone@gmail.com>
2023-02-14 13:16:14 +0300

    feature: add new items

--- src/pages/settings/Settings.style.ts
+++ src/pages/settings/Settings.style.ts
@@ -3,9 +3,30 @@ import styled from "styled-components";
 export const Elem = styled.div``;
 
 export const SettingsWrapper = styled.div`
+  /* height: 100%;
+  width: 100%; */
+  display: flex;
+  flex-direction: column;
+  row-gap: 25px;
+`;
+
+export const SettingsItems = styled.div`
+  padding-left: 20px;
+  padding-right: 20px;
   height: 100%;
   width: 100%;
   display: flex;
   flex-direction: column;
-  row-gap: 25px;
+  row-gap: 15px;
+`;
+
+export const Settingsitem = styled.div`
+  padding: 5px;
+  background-color: #383b5f;
+  border-radius: 10px;
+`;
+
+export const SettingsExit = styled.div`
+  padding-left: 20px;
+  padding-right: 20px;
 `;
