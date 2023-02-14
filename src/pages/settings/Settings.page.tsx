9eedeab1ebbf3354317c3d8601a6cd2f358af334
XCrone <ls.xcrone@gmail.com>
2023-02-14 13:16:03 +0300

    feature: add new items

--- src/pages/settings/Settings.page.tsx
+++ src/pages/settings/Settings.page.tsx
@@ -1,25 +1,41 @@
 import HeaderComponent, { IButtonHeader } from "../../components/header/Header";
-import { SettingsWrapper } from "./Settings.style";
-
-interface Props {}
+import EmailComponent from "../../components/settings/Email.component";
+import ToggleComponent from "../../components/settings/Toggle.component";
+import { useAppDispatch } from "../../hooks/redux";
+import { logOut } from "../../store/slices/auth.slice";
+import { GButton } from "../../style/components/button.style";
+import { SettingsExit, Settingsitem, SettingsItems, SettingsWrapper } from "./Settings.style";
 
 const SettingsPage = () => {
-  const callbackPlus = () => {};
-  const callbackSearch = () => {};
+  const dispatch = useAppDispatch();
+
   const buttonsHeader: IButtonHeader[] = [
     {
-      callback: callbackPlus,
-      icon: "bi bi-search",
+      callback: () => {},
+      icon: "",
     },
     {
-      callback: callbackSearch,
-      icon: "bi bi-plus-lg",
+      callback: () => {},
+      icon: "",
     },
   ];
 
   return (
     <SettingsWrapper>
       <HeaderComponent buttns={buttonsHeader} title={"settings"} />
+      <SettingsItems>
+        <Settingsitem>
+          <EmailComponent />
+        </Settingsitem>
+        <Settingsitem>
+          <ToggleComponent />
+        </Settingsitem>
+      </SettingsItems>
+      <SettingsExit>
+        <GButton onClick={() => dispatch(logOut())} type="submit" color1="#d72626" color2="#eb4d4d">
+          logout
+        </GButton>
+      </SettingsExit>
     </SettingsWrapper>
   );
 };
