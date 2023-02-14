e5ba5767b6df380d5685653916794896f87fdeb6
XCrone <ls.xcrone@gmail.com>
2023-02-14 13:11:21 +0300

    feature: join global button

---
+++ src/style/components/button.style.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const GButton = styled.button<{ color1: string; color2: string }>`
+  height: 50px;
+  width: 100%;
+  background: ${(props) => props.color1};
+  background: linear-gradient(142deg, ${(props) => props.color2} 0%, ${(props) => props.color1} 100%);
+  text-transform: capitalize;
+  font-size: 14px;
+  line-height: 19px;
+  margin-bottom: 10px;
+`;
