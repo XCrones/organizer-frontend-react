0ae83f9c0701dcd425c7550be89af5d2599ef791
XCrone <ls.xcrone@gmail.com>
2023-02-15 13:44:37 +0300

    feature: join create Thunk

--- src/store/slices/calendar.slice.ts
+++ src/store/slices/calendar.slice.ts
@@ -1,10 +1,11 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "../../axios";
-import { IEvent } from "../../models/calendar.models";
+import { IEvent, IJoinEvent } from "../../models/calendar.models";
 
 interface IStateInitial {
   pending: {
     getAll: boolean;
+    join: boolean;
   };
   events: IEvent[];
 }
@@ -12,6 +13,7 @@ interface IStateInitial {
 const initialStateValue: IStateInitial = {
   pending: {
     getAll: false,
+    join: false,
   },
   events: [],
 };
@@ -19,7 +21,7 @@ const initialStateValue: IStateInitial = {
 export const fetchEvents = createAsyncThunk<IEvent[], undefined, { fullFilled: IEvent[]; rejectValue: void }>(
   "calendar/fetchEvents",
   async function (_, { fulfillWithValue, rejectWithValue }) {
-    const { data } = await axios.get<IEvent[]>("calendar", {
+    const { data } = await axios.get("calendar", {
       data: {
         uid: 1,
       },
@@ -27,6 +29,21 @@ export const fetchEvents = createAsyncThunk<IEvent[], undefined, { fullFilled: I
     if (!!data) {
       return fulfillWithValue(data);
     }
+    return rejectWithValue();
+  }
+);
+
+export const joinEvent = createAsyncThunk<IEvent, IJoinEvent, { fullFilled: IEvent; rejectValue: void }>(
+  "calendar/joinNew",
+  async function (event, { fulfillWithValue, rejectWithValue }) {
+    const { data } = await axios.post<IEvent>("calendar", {
+      ...event,
+    });
+
+    if (!!data) {
+      fulfillWithValue(data);
+    }
+
     return rejectWithValue();
   }
 );
@@ -43,9 +60,21 @@ export const Calenadar = createSlice({
       .addCase(fetchEvents.fulfilled, (state, action) => {
         state.pending.getAll = false;
         state.events = action.payload;
+        // console.log(action.payload);
       })
       .addCase(fetchEvents.rejected, (state) => {
         state.pending.getAll = false;
+      })
+      //
+      .addCase(joinEvent.pending, (state) => {
+        state.pending.join = true;
+      })
+      .addCase(joinEvent.fulfilled, (state, action) => {
+        state.pending.join = false;
+        // console.log(typeof action.payload);
+      })
+      .addCase(joinEvent.rejected, (state) => {
+        state.pending.join = false;
       });
   },
 });
