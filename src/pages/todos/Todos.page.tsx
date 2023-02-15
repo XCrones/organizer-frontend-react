6f787673fed21c3669405b75614058b129cc3bbb
XCrone <ls.xcrone@gmail.com>
2023-02-15 15:52:01 +0300

    feature: add  wrapper & create component

--- src/pages/todos/Todos.page.tsx
+++ src/pages/todos/Todos.page.tsx
@@ -1,20 +1,22 @@
 import TodoListComponent from "../../components/todos/TodoList.component";
 import TodoExampleComponent from "../../components/todos/TodoExample.component";
-import { Examples, Items, TodosWrapper } from "./Todos.style";
+import { Examples, Items, Todos, TodosWrapper } from "./Todos.style";
 import HeaderComponent, { IButtonHeader } from "../../components/header/Header";
+import { useState } from "react";
+import CreateTodoComponent from "./CreateTodo.component";
 
 interface Props {}
 
 const TodosPage = () => {
-  const callbackPlus = () => {};
-  const callbackSearch = () => {};
+  const [isHideCreate, SetHideCreate] = useState(true);
+
   const buttonsHeader: IButtonHeader[] = [
     {
-      callback: callbackPlus,
+      callback: () => {},
       icon: "bi bi-search",
     },
     {
-      callback: callbackSearch,
+      callback: () => SetHideCreate(false),
       icon: "bi bi-plus-lg",
     },
   ];
@@ -22,12 +24,15 @@ const TodosPage = () => {
   return (
     <TodosWrapper>
       <HeaderComponent buttns={buttonsHeader} title={"toDo"} />
-      <Examples>
-        <TodoExampleComponent />
-      </Examples>
-      <Items>
-        <TodoListComponent />
-      </Items>
+      <Todos>
+        <Examples>
+          <TodoExampleComponent />
+        </Examples>
+        <Items>
+          <TodoListComponent />
+        </Items>
+        {!isHideCreate && <CreateTodoComponent callbackClose={() => SetHideCreate(true)} title="todo" />}
+      </Todos>
     </TodosWrapper>
   );
 };
