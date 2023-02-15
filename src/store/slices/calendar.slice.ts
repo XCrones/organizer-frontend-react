767f530256014ff200f78d70af290a2b307c95c2
XCrone <ls.xcrone@gmail.com>
2023-02-15 14:01:46 +0300

    fix bug & types fetch all

--- src/store/slices/calendar.slice.ts
+++ src/store/slices/calendar.slice.ts
@@ -21,7 +21,7 @@ const initialStateValue: IStateInitial = {
 export const fetchEvents = createAsyncThunk<IEvent[], undefined, { fullFilled: IEvent[]; rejectValue: void }>(
   "calendar/fetchEvents",
   async function (_, { fulfillWithValue, rejectWithValue }) {
-    const { data } = await axios.get("calendar", {
+    const { data } = await axios.get<IEvent[]>("calendar", {
       data: {
         uid: 1,
       },
@@ -41,7 +41,7 @@ export const joinEvent = createAsyncThunk<IEvent, IJoinEvent, { fullFilled: IEve
     });
 
     if (!!data) {
-      fulfillWithValue(data);
+      return fulfillWithValue(data);
     }
 
     return rejectWithValue();
@@ -60,7 +60,6 @@ export const Calenadar = createSlice({
       .addCase(fetchEvents.fulfilled, (state, action) => {
         state.pending.getAll = false;
         state.events = action.payload;
-        // console.log(action.payload);
       })
       .addCase(fetchEvents.rejected, (state) => {
         state.pending.getAll = false;
@@ -71,7 +70,7 @@ export const Calenadar = createSlice({
       })
       .addCase(joinEvent.fulfilled, (state, action) => {
         state.pending.join = false;
-        // console.log(typeof action.payload);
+        state.events = [...state.events, action.payload];
       })
       .addCase(joinEvent.rejected, (state) => {
         state.pending.join = false;
