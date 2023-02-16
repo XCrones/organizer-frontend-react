0a063f3c8bd68c7ae720005a5643fff91351fe60
XCrone <ls.xcrone@gmail.com>
2023-02-16 17:40:18 +0300

    refactor: delete trash

--- src/components/calendar/Shedule.component.tsx
+++ src/components/calendar/Shedule.component.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { IEvent, IParseEvent } from "../../models/calendar.models";
+import { IParseEvent } from "../../models/calendar.models";
 import {
   EventItem,
   Events,
