0723ad42e298d3f2fb4e0eb1654e7d0aa1b47462
XCrone <ls.xcrone@gmail.com>
2023-02-16 12:19:14 +0300

    feature: add totay & tommorow & refactoring

--- src/components/Todos/List.component.tsx
+++ src/components/Todos/List.component.tsx
@@ -13,6 +13,14 @@ interface IParsingTodo {
 }
 
 interface ITodos {
+  today: {
+    title: "today";
+    todos: ITodo[];
+  };
+  tommorow: {
+    title: "tommorow";
+    todos: ITodo[];
+  };
   thisMonth: {
     title: "this month";
     todos: ITodo[];
@@ -31,8 +39,6 @@ const TodosListComponent = () => {
   const dispatch = useAppDispatch();
   const todos = useAppSelector((state) => state.todos.todos);
 
-  const [list, SetList] = useState<IParsingTodo[]>([]);
-
   const toggleStatus = (item: ITodo) => {
     const todoItem = JSON.parse(JSON.stringify(item)) as ITodo;
     todoItem.status = !todoItem.status;
@@ -50,9 +56,19 @@ const TodosListComponent = () => {
     dispatch(fetchTodos());
   }, []);
 
-  const parseTodos = (todoItems: ITodo[]) => {
+  const parseTodos = (todoItems: ITodo[]): IParsingTodo[] => {
+    const sortArr = (arr: ITodo[]) => arr.sort((a, b) => (a.deadline < b.deadline ? -1 : 1));
+
     try {
       const todos: ITodos = {
+        today: {
+          title: "today",
+          todos: [],
+        },
+        tommorow: {
+          title: "tommorow",
+          todos: [],
+        },
         thisMonth: {
           title: "this month",
           todos: [],
@@ -70,34 +86,63 @@ const TodosListComponent = () => {
       const localeDate = {
         year: +new Date().toLocaleDateString().split(".")[2],
         month: +new Date().toLocaleDateString().split(".")[1],
+        day: +new Date().toLocaleDateString().split(".")[0],
+        full: new Date().toLocaleDateString(),
       };
 
       const parseDate = (date: string) => {
-        const makeDate = new Date(Date.parse(date)).toLocaleDateString().split(".");
+        const makeDate = new Date(Date.parse(date)).toLocaleDateString();
         return {
-          year: +makeDate[2],
-          month: +makeDate[1],
+          year: +makeDate.split(".")[2],
+          month: +makeDate.split(".")[1],
+          day: +makeDate.split(".")[0],
+          full: makeDate,
         };
       };
 
       todoItems.forEach((todo) => {
-        if (parseDate(todo.deadline).year > localeDate.year) {
-          todos.more.todos.push(todo);
-        } else if (parseDate(todo.deadline).month === localeDate.month) {
+        if (parseDate(todo.deadline).full === localeDate.full) {
+          todos.today.todos.push(todo);
+        } else if (
+          parseDate(todo.deadline).month === localeDate.month &&
+          parseDate(todo.deadline).day === localeDate.day + 1
+        ) {
+          todos.tommorow.todos.push(todo);
+        } else if (
+          parseDate(todo.deadline).month === localeDate.month &&
+          parseDate(todo.deadline).year === localeDate.year
+        ) {
           todos.thisMonth.todos.push(todo);
-        } else if (parseDate(todo.deadline).year === localeDate.year) {
+        } else if (
+          parseDate(todo.deadline).year === localeDate.year &&
+          parseDate(todo.deadline).month >= localeDate.month
+        ) {
           todos.thisYear.todos.push(todo);
+        } else if (parseDate(todo.deadline).year > localeDate.year) {
+          todos.more.todos.push(todo);
         }
       });
 
-      todos.thisMonth.todos.sort((a, b) => (a.deadline < b.deadline ? -1 : 1));
-      todos.thisYear.todos.sort((a, b) => (a.deadline < b.deadline ? -1 : 1));
-      todos.more.todos.sort((a, b) => (a.deadline < b.deadline ? -1 : 1));
+      todos.today.todos = sortArr(todos.today.todos);
+      todos.tommorow.todos = sortArr(todos.tommorow.todos);
+      todos.thisMonth.todos = sortArr(todos.thisMonth.todos);
+      todos.thisYear.todos = sortArr(todos.thisYear.todos);
+      todos.more.todos = sortArr(todos.more.todos);
 
-      return todos;
+      return Object.values(todos);
     } catch (err) {
-      console.log(err);
-      return {
+      console.log("err", err);
+
+      const sortingData = sortArr(JSON.parse(JSON.stringify(todoItems)));
+      const todos: ITodos = {
+        today: {
+          title: "today",
+          todos: [],
+        },
+        tommorow: {
+          title: "tommorow",
+          todos: [],
+        },
         thisMonth: {
           title: "this month",
           todos: [],
@@ -108,25 +153,19 @@ const TodosListComponent = () => {
         },
         more: {
           title: "more",
-          todos: todoItems,
+          todos: sortingData,
         },
       };
+      return Object.values(todos);
     }
   };
 
-  const memoizeList = useMemo(() => parseTodos(todos), [todos]);
-
-  useEffect(() => {
-    if (todos.length > 0) {
-      SetList(Object.values(memoizeList));
-      console.log(memoizeList);
-    }
-  }, [todos]);
+  const memoizeList = useMemo(() => parseTodos(todos) as IParsingTodo[], [todos]);
 
   return (
     <ListItems>
-      {list.map((item) => (
-        <ListItem key={item.title}>
+      {memoizeList.map((item) => (
+        <ListItem isHide={item.todos.length === 0} key={item.title}>
           <ListTitle>{item.title}</ListTitle>
           {item.todos.map((todo) => (
             <TodoItemComponent key={todo.id} item={todo} callbackToggle={toggleStatus} callbackEdit={editItem} />
