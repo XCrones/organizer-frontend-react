c1da801325491cd0efec8034e3b13c1710b31bc4
XCrone <ls.xcrone@gmail.com>
2023-02-16 11:27:14 +0300

    feature: add callback edit

--- src/components/Todos/ListItem.component.tsx
+++ src/components/Todos/ListItem.component.tsx
@@ -2,15 +2,15 @@ import { useMemo } from "react";
 import { ITodo } from "../../models/todos.models";
 import { GCheckboxItem, GCheckboxReplace } from "../../style/components/checkbox.style";
 import { color } from "../../style/variables.style";
-import { Item, ItemDate, ItemInfo, ItemTitle, ItemTriangle } from "./ListItem.style";
+import { Item, ItemDate, ItemEdit, ItemInfo, ItemTitle, ItemTriangle } from "./ListItem.style";
 
 interface Props {
   item: ITodo;
-  toggleStatus: Function;
-  background: string;
+  callbackToggle: Function;
+  callbackEdit: Function;
 }
 
-const TodoItemComponent = ({ item, toggleStatus, background }: Props) => {
+const TodoItemComponent = ({ item, callbackEdit, callbackToggle }: Props) => {
   const parseDate = (date: string): string => {
     const parseDate = new Date(Date.parse(date)).toLocaleDateString();
     return parseDate;
@@ -36,13 +36,16 @@ const TodoItemComponent = ({ item, toggleStatus, background }: Props) => {
   return (
     <Item bgColor={color.todoItem}>
       <GCheckboxItem size={20}>
-        <input onClick={() => toggleStatus(item)} type="checkbox" />
-        <GCheckboxReplace colorSelect={background} isSelect={item.status} rounded={25} />
+        <input onClick={() => callbackToggle(item)} type="checkbox" />
+        <GCheckboxReplace colorSelect={item.background} isSelect={item.status} rounded={15} />
       </GCheckboxItem>
       <ItemInfo>
         <ItemTitle>{item.title}</ItemTitle>
         <ItemDate>{memoizeDate}</ItemDate>
       </ItemInfo>
+      <ItemEdit color={item.background} onClick={() => callbackEdit(item)} type="button">
+        <i className="bi bi-info-square"></i>
+      </ItemEdit>
       <ItemTriangle background={memoizePriority} />
     </Item>
   );
