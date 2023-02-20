f894b19fdcaa6e01dcac51e076e058bb92d98a00
XCrone <ls.xcrone@gmail.com>
2023-02-20 13:16:01 +0300

    feature: add persist

--- src/store/settings.store.ts
+++ src/store/settings.store.ts
@@ -1,14 +1,22 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 interface SettingsStore {
   isNightTheme: boolean;
   toggleTheme: () => void;
 }
-
-export const useSettingsStore = create<SettingsStore>()((set, get) => ({
-  isNightTheme: true,
-  toggleTheme: () =>
-    set({
-      isNightTheme: !get().isNightTheme,
+export const useSettingsStore = create<SettingsStore>()(
+  persist(
+    (set, get) => ({
+      isNightTheme: true,
+      toggleTheme: () =>
+        set({
+          isNightTheme: !get().isNightTheme,
+        }),
     }),
-}));
+    {
+      name: "settings",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
