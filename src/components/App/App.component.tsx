113e48828431942e0015c749f0d6d4f724551d28
XCrone <ls.xcrone@gmail.com>
2023-02-22 09:14:35 +0300

    feature: move routes to RoutesComponent

--- src/components/App/App.component.tsx
+++ src/components/App/App.component.tsx
@@ -1,16 +1,10 @@
-import TodosPage from "../../pages/todos/Todos.page";
-import CalendarPage from "../../pages/calendar/Calendar.page";
-import SettingsPage from "../../pages/settings/Settings.page";
-import WeatherPage from "../../pages/weather/Weather.page";
-import AuthPage from "../../pages/auth/Auth.page";
 import FooterComponent from "../footer/Footer.component";
-import { Routes, Route } from "react-router-dom";
 import { Footer, Section, Wrapper } from "./App.style";
 import { color } from "../../style/variables.style";
 import { useWindowSize } from "../../hooks/windowResize";
 import { useAuthStore } from "../../store/auth.store";
-import { ROUTER_LINKS } from "../../router-links";
 import style from "./App.module.scss";
+import RoutesComponent from "../routes/Routes.component";
 
 const App = () => {
   const isAuth = useAuthStore((state) => state.isAuth);
@@ -22,13 +16,7 @@ const App = () => {
   return (
     <Wrapper className={style.scroll} colorBg={color.mainBg} height={{ footer: HEIGHT_FOOTER }}>
       <Section maxHeight={memoizedHeight} paddingBottom={PADDING_BOTTOM}>
-        <Routes>
-          <Route path={ROUTER_LINKS.auth.link} element={<AuthPage />} />
-          <Route path={ROUTER_LINKS.todos.link} element={<TodosPage />} />
-          <Route path={ROUTER_LINKS.calendar.link} element={<CalendarPage />} />
-          <Route path={ROUTER_LINKS.wather.link} element={<WeatherPage />} />
-          <Route path={ROUTER_LINKS.settings.link} element={<SettingsPage />} />
-        </Routes>
+        <RoutesComponent />
       </Section>
       {isAuth && (
         <Footer>
