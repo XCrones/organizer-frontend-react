f9b2821df40245809a96d831c8d1bfca9905706c
XCrone <ls.xcrone@gmail.com>
2023-02-22 09:24:23 +0300

    refact: remove callback

--- src/pages/auth/Auth.page.tsx
+++ src/pages/auth/Auth.page.tsx
@@ -1,20 +1,15 @@
 import { useState } from "react";
 import AuthSignInComponent from "../../components/auth/AuthSignIn.component";
 import AuthSignUpComponent from "../../components/auth/AuthSignUp.component";
-import { IAuth } from "../../models/auth.model";
 import { AuthWrapper } from "./Auth.style";
 
 const AuthPage = () => {
   const [isSignIn, toggleForm] = useState(true);
 
-  const callbackSubmit = (user: IAuth) => {
-    console.log(user);
-  };
-
   return (
     <AuthWrapper>
-      {isSignIn && <AuthSignInComponent toggleForm={() => toggleForm(!isSignIn)} callback={callbackSubmit} />}
-      {!isSignIn && <AuthSignUpComponent toggleForm={() => toggleForm(!isSignIn)} callback={callbackSubmit} />}
+      {isSignIn && <AuthSignInComponent toggleForm={() => toggleForm(!isSignIn)} />}
+      {!isSignIn && <AuthSignUpComponent toggleForm={() => toggleForm(!isSignIn)} />}
     </AuthWrapper>
   );
 };
