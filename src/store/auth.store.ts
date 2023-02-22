487b39d9bc2d3b9b3505753aea66e96f97ff1fd3
XCrone <ls.xcrone@gmail.com>
2023-02-22 11:28:32 +0300

    ref:rm isAuth userData is persist

--- src/store/auth.store.ts
+++ src/store/auth.store.ts
@@ -1,5 +1,6 @@
 import { AxiosError } from "axios";
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 import axios from "../axios";
 import { IAuthSignIn, IAuthSignUp } from "../models/auth.model";
 
@@ -16,79 +17,85 @@ interface ISignErr {
 }
 
 interface AuthStore {
-  userData: IUserData | undefined;
-  isAuth: boolean;
+  userData: IUserData | null;
   isPending: boolean;
   singIn: (userData: IAuthSignIn) => Promise<ISignErr>;
   singUp: (userData: IAuthSignUp) => Promise<ISignErr>;
   logOut: () => Promise<void>;
 }
 
-export const useAuthStore = create<AuthStore>()((set, get) => ({
-  userData: undefined,
-  isAuth: false,
-  isPending: false,
-  singIn: async (userData) => {
-    set({ isPending: true });
-    try {
-      const { data } = await axios.post<IUserData>("auth/signin", userData, {
-        headers: {
-          "Conent-type": "Application/json",
-        },
-      });
+export const useAuthStore = create<AuthStore>()(
+  persist(
+    (set) => ({
+      userData: null,
+      isPending: false,
+      singIn: async (userData) => {
+        set({ isPending: true });
+        try {
+          const { data } = await axios.post<IUserData>("auth/signin", userData, {
+            headers: {
+              "Conent-type": "Application/json",
+            },
+          });
 
-      if (!!data) {
-        set({ userData: { ...JSON.parse(JSON.stringify(data)) } });
-      }
+          if (!!data) {
+            set({ userData: { ...JSON.parse(JSON.stringify(data)) } });
+          }
 
-      set({ isAuth: true });
-      return Promise.resolve({ isError: false, message: "" });
-    } catch (error) {
-      const err = error as AxiosError<{ statusCode: number; message: string; error: string }>;
-      if (!!err.response?.data) {
-        return { isError: true, message: err.response.data.message };
-      }
-      return Promise.reject(err.message);
-    } finally {
-      set({ isPending: false });
+          return Promise.resolve({ isError: false, message: "" });
+        } catch (error) {
+          const err = error as AxiosError<{ statusCode: number; message: string; error: string }>;
+          if (!!err.response?.data) {
+            return { isError: true, message: err.response.data.message };
+          }
+          return Promise.reject(err.message);
+        } finally {
+          set({ isPending: false });
+        }
+      },
+      singUp: async (userData) => {
+        set({ isPending: true });
+        try {
+          const { data } = await axios.post<IUserData>("auth/signup", userData, {
+            headers: {
+              "Conent-type": "Application/json",
+            },
+          });
+
+          if (!!data) {
+            set({ userData: { ...JSON.parse(JSON.stringify(data)) } });
+          }
+
+          return Promise.resolve({ isError: false, message: "" });
+        } catch (error) {
+          const err = error as AxiosError<{ statusCode: number; message: string; error: string }>;
+          if (!!err.response?.data) {
+            return { isError: true, message: err.response.data.message };
+          }
+          return Promise.reject(err.message);
+        } finally {
+          set({ isPending: false });
+        }
+      },
+      logOut: async () => {
+        set({ isPending: true });
+        try {
+          set({
+            userData: null,
+          });
+        } catch (error) {
+          return Promise.reject(error);
+        } finally {
+          set({ isPending: false });
+        }
+      },
+    }),
+    {
+      name: "organizer",
+      storage: createJSONStorage(() => sessionStorage),
+      partialize: (state) => ({
+        userData: state.userData,
+      }),
     }
-  },
-  singUp: async (userData) => {
-    set({ isPending: true });
-    try {
-      const { data } = await axios.post<IUserData>("auth/signup", userData, {
-        headers: {
-          "Conent-type": "Application/json",
-        },
-      });
-
-      if (!!data) {
-        set({ userData: { ...JSON.parse(JSON.stringify(data)) } });
-      }
-
-      set({ isAuth: true });
-      return Promise.resolve({ isError: false, message: "" });
-    } catch (error) {
-      const err = error as AxiosError<{ statusCode: number; message: string; error: string }>;
-      if (!!err.response?.data) {
-        return { isError: true, message: err.response.data.message };
-      }
-      return Promise.reject(err.message);
-    } finally {
-      set({ isPending: false });
-    }
-  },
-  logOut: async () => {
-    set({ isPending: true });
-    try {
-      set({
-        isAuth: false,
-        userData: undefined,
-      });
-    } catch (error) {
-      return Promise.reject(error);
-    } finally {
-      set({ isPending: false });
-    }
-  },
-}));
+  )
+);
