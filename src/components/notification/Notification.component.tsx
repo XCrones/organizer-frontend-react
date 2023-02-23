839e7f443445bcff79dd3d1a6456025f4d33bc80
XCrone <ls.xcrone@gmail.com>
2023-02-23 12:30:48 +0300

    create component

--- src/components/notification/Notification.component.tsx
+++ src/components/notification/Notification.component.tsx
@@ -1,9 +1,28 @@
-import { Notification } from "./Notification.style";
+import ReactDOM from "react-dom";
+import { INotifMeta } from "../../models";
+import { Notification, NotificationTitle } from "./Notification.style";
 
-interface Props {}
+interface Props {
+  meta: INotifMeta;
+  isHide: boolean;
+}
 
-const NotificationComponent = () => {
-  return <Notification></Notification>;
+const NotificationComponent = ({ meta, isHide }: Props) => {
+  if (isHide) return null;
+  return ReactDOM.createPortal(
+    <Notification isHide={isHide} background={meta.background}>
+      {Array.isArray(meta.title) ? (
+        meta.title.map((error, idx) => (
+          <NotificationTitle key={idx} style={{ textAlign: "left", fontSize: "16px" }}>
+            {idx + 1}) {error}
+          </NotificationTitle>
+        ))
+      ) : (
+        <NotificationTitle style={{ textAlign: "center", fontSize: "16px" }}>{meta.title}</NotificationTitle>
+      )}
+    </Notification>,
+    document.body
+  );
 };
 
 export default NotificationComponent;
--- src/components/notification/Notification.style.ts
+++ src/components/notification/Notification.style.ts
@@ -1,3 +1,30 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-export const Notification = styled.div``;
+const showNotif = keyframes`
+  from {
+    transform: translateY(-100%);
+  }
+
+  to {
+    transform: translateY(0%);
+  }
+`;
+
+export const Notification = styled.div<{ background: string; isHide: boolean }>`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  padding: 16px 0;
+  background-color: ${(props) => props.background};
+  color: #fff;
+  z-index: 100;
+  animation: ${showNotif} 200ms linear;
+`;
+
+export const NotificationTitle = styled.h2`
+  padding: 5px 10px;
+  &::first-letter {
+    text-transform: capitalize;
+  }
+`;
