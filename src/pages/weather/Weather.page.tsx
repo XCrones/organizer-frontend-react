3282078ed043a29d2ed4f60cc2584f00797f8ce1
XCrone <ls.xcrone@gmail.com>
2023-03-08 13:39:59 +0300

    join: forecast

--- src/pages/weather/Weather.page.tsx
+++ src/pages/weather/Weather.page.tsx
@@ -1,26 +1,73 @@
-import { HeaderComponent } from "../../components";
+import { useEffect, useState } from "react";
+import { HeaderComponent, WeatherForecastComponent } from "../../components";
 import { IHeaderButton } from "../../models";
-import { WeatherWrapper } from "./Weather.style";
-
-interface Props {}
+import { WeatherButn, CityName, WeatherCities, WeatherCity, WeatherContent, WeatherWrapper } from "./Weather.style";
+import { shallow } from "zustand/shallow";
+import { useWeatherStore } from "../../store";
 
 const WeatherPage = () => {
-  const callbackPlus = () => {};
-  const callbackSearch = () => {};
+  const [cityName, SetCityName] = useState("");
+  const [isHideForecast, SetHideForecast] = useState(true);
+
+  const weatherStore = useWeatherStore(
+    (state) => ({
+      cities: state.data.cities,
+      getAllData: state.getCities,
+      dropCity: state.dropCity,
+    }),
+    shallow
+  );
+
   const buttonsHeader: IHeaderButton[] = [
     {
-      callback: callbackPlus,
-      icon: "bi bi-search",
+      callback: () => {},
+      icon: "",
     },
     {
-      callback: callbackSearch,
+      callback: () => {},
       icon: "bi bi-plus-lg",
     },
   ];
 
+  useEffect(() => {
+    weatherStore.getAllData();
+    console.log("get all data");
+  }, []);
+
+  console.log("weather page");
+
+  const showForecast = (cityName: string) => {
+    SetCityName(cityName);
+    SetHideForecast(false);
+  };
+
   return (
     <WeatherWrapper>
       <HeaderComponent buttns={buttonsHeader} title={"weather"} />
+      {isHideForecast && (
+        <WeatherContent>
+          <WeatherCities>
+            {weatherStore.cities.map((city) => (
+              <WeatherCity key={city.id}>
+                <WeatherButn background="#3794b1" onClick={() => showForecast(city.name)}>
+                  <i className="bi bi-info-circle"></i>
+                </WeatherButn>
+                <CityName>
+                  {city.name}, {city.country}
+                </CityName>
+                <WeatherButn background="#ff0000" onClick={() => weatherStore.dropCity(city.id)}>
+                  <i className="bi bi-x-lg"></i>
+                </WeatherButn>
+              </WeatherCity>
+            ))}
+          </WeatherCities>
+        </WeatherContent>
+      )}
+      <WeatherForecastComponent
+        cityName={cityName}
+        isHide={isHideForecast}
+        callbackClose={() => SetHideForecast(true)}
+      />
     </WeatherWrapper>
   );
 };
