2f859fd0833dc8613b62ad61e5118dd55aa15acf
XCrone <ls.xcrone@gmail.com>
2023-03-08 13:40:16 +0300

    join: components forecast

--- src/pages/weather/Weather.style.ts
+++ src/pages/weather/Weather.style.ts
@@ -1,11 +1,56 @@
 import styled from "styled-components";
+import { GColor, GFonts } from "../../style/variables.style";
 
 export const Elem = styled.div``;
 
 export const WeatherWrapper = styled.div`
-  height: 100%;
-  width: 100%;
+  font-family: ${GFonts.inter};
   display: flex;
   flex-direction: column;
-  row-gap: 25px;
+  row-gap: 10px;
+  position: relative;
+`;
+
+export const WeatherContent = styled.div`
+  padding-left: 20px;
+  padding-right: 20px;
+`;
+
+export const WeatherCities = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 15px;
+`;
+
+export const WeatherCity = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  background-color: ${GColor.todoItem};
+  padding: 15px;
+  border-radius: 11px;
+`;
+
+export const CityName = styled.div<{}>`
+  flex: 1 1 auto;
+  font-size: 18px;
+  font-family: ${GFonts.inter};
+  text-transform: capitalize;
+  padding-left: 10px;
+`;
+
+export const WeatherButn = styled.button.attrs({
+  type: "button",
+})<{ background: string }>`
+  padding: 5px;
+  border-radius: 7px;
+  transition: all 150ms ease-in;
+  color: #808080;
+  font-size: 18px;
+
+  &:hover {
+    color: #fff;
+    background-color: ${(props) => props.background};
+  }
 `;
