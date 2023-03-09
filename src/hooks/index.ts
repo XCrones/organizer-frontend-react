5528fb1423749474d8aa0a127475c56aa83e4d52
XCrone <ls.xcrone@gmail.com>
2023-03-09 17:49:44 +0300

    add: useGeolocation

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,6 @@
 import { useWindowSize } from "./windowResize";
 import { useDate } from "./date";
 import { useNotif } from "./notification";
+import { useGeolocation } from "./geolocation";
 
-export { useDate, useWindowSize, useNotif };
+export { useDate, useWindowSize, useNotif, useGeolocation };
