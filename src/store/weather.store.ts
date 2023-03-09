30fa00f2959a66c2de6efa4d2ddc19bbaf418b23
XCrone <ls.xcrone@gmail.com>
2023-03-09 17:59:01 +0300

    rename: methods. add: joinCityByGeo, cityMove

--- src/store/weather.store.ts
+++ src/store/weather.store.ts
@@ -2,7 +2,7 @@ import { AxiosError } from "axios";
 import { create } from "zustand";
 import { Axios } from "../config/axios/methods";
 import { ROUTES } from "../config/routes/routes";
-import { ICityWeather, IPending, IReqForecastWeather, IWeatherForecast } from "../models";
+import { ICityWeather, IPending, IReqWeatherByName, IReqWeatherByGeo, IWeatherForecast, IMoveItem } from "../models";
 
 interface IData {
   forecast: IWeatherForecast | null;
@@ -13,10 +13,12 @@ interface IStateInitial {
   endPoint: string;
   pending: IPending;
   data: IData;
-  getCities: () => void;
-  getForecast: (nameCity: string) => void;
-  joinCity: (nameCity: string) => void;
-  dropCity: (idCIty: number) => void;
+  getCities: () => Promise<void>;
+  getForecast: (id: number) => Promise<void>;
+  joinCityByName: (nameCity: string) => Promise<void>;
+  joinCityByGeo: (reqData: IReqWeatherByGeo) => Promise<void>;
+  dropCity: (idCIty: number) => Promise<void>;
+  cityMove: (meta: IMoveItem) => void;
 }
 
 export const useWeatherStore = create<IStateInitial>()((set, get) => ({
@@ -32,7 +34,7 @@ export const useWeatherStore = create<IStateInitial>()((set, get) => ({
   getCities: async () => {
     set({ pending: { ...get().pending, fetchAll: true } });
     try {
-      const data = await Axios.get<ICityWeather[]>({ path: `${get().endPoint}/cities` });
+      const data = await Axios.get<ICityWeather[]>({ path: `${get().endPoint}` });
       if (!!data) {
         set({ data: { ...get().data, cities: data } });
       }
@@ -44,15 +46,51 @@ export const useWeatherStore = create<IStateInitial>()((set, get) => ({
       set({ pending: { ...get().pending, fetchAll: false } });
     }
   },
-  getForecast: async (nameCity) => {
+  joinCityByName: async (nameCity) => {
     set({ pending: { ...get().pending, fetchOne: true } });
     try {
-      const req: IReqForecastWeather = {
+      const reqData: IReqWeatherByName = {
         city: nameCity,
       };
-      const data = await Axios.post<IWeatherForecast, IReqForecastWeather>({
-        path: `${get().endPoint}/forecast`,
-        data: req,
+
+      const data = await Axios.post<ICityWeather[], IReqWeatherByName>({
+        path: `${get().endPoint}/city-name`,
+        data: reqData,
+      });
+      if (!!data) {
+        set({ data: { ...get().data, cities: data } });
+      }
+    } catch (error) {
+      const err = error as AxiosError;
+      console.log(err);
+      throw err;
+    } finally {
+      set({ pending: { ...get().pending, fetchOne: false } });
+    }
+  },
+  joinCityByGeo: async (reqData) => {
+    set({ pending: { ...get().pending, fetchOne: true } });
+    try {
+      const data = await Axios.post<ICityWeather[], IReqWeatherByGeo>({
+        path: `${get().endPoint}/city-geo`,
+        data: reqData,
+      });
+      if (!!data) {
+        set({ data: { ...get().data, cities: data } });
+      }
+    } catch (error) {
+      const err = error as AxiosError;
+      console.log(err);
+      throw err;
+    } finally {
+      set({ pending: { ...get().pending, fetchOne: false } });
+    }
+  },
+  getForecast: async (id) => {
+    set({ pending: { ...get().pending, fetchOne: true } });
+    try {
+      const data = await Axios.get<IWeatherForecast>({
+        path: `${get().endPoint}/${id}`,
       });
       if (!!data) {
         set({ data: { ...get().data, forecast: data } });
@@ -65,7 +103,6 @@ export const useWeatherStore = create<IStateInitial>()((set, get) => ({
       set({ pending: { ...get().pending, fetchOne: false } });
     }
   },
-  joinCity: (nameCity) => {},
   dropCity: async (idCity) => {
     set({ pending: { ...get().pending, fetchOne: true } });
     try {
@@ -80,6 +117,26 @@ export const useWeatherStore = create<IStateInitial>()((set, get) => ({
       throw err;
     } finally {
       set({ pending: { ...get().pending, fetchOne: false } });
+    }
+  },
+  cityMove: (meta) => {
+    try {
+      const arrCities = JSON.parse(JSON.stringify(get().data.cities));
+
+      if (!!arrCities) {
+        const firstElemIdx = get().data.cities.findIndex((city) => city.id === meta.id);
+        const firstElem = get().data.cities.find((city) => city.id === meta.id);
+        const secondElem = get().data.cities[meta.index];
+
+        if (firstElemIdx >= 0 && !!firstElem && !!secondElem) {
+          arrCities[firstElemIdx] = secondElem;
+          arrCities[meta.index] = firstElem;
+          set({ data: { ...get().data, cities: arrCities } });
+          // ! update back
+        }
+      }
+    } catch (error) {
+      throw error;
     }
   },
 }));
