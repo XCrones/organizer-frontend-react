da34508f59d11148b0cc8c096c71a77339dc7f47
XCrone <ls.xcrone@gmail.com>
2023-03-09 17:54:06 +0300

    rename: WrapperWeather

--- src/pages/weather/Weather.style.ts
+++ src/pages/weather/Weather.style.ts
@@ -1,9 +1,7 @@
 import styled from "styled-components";
 import { GFonts } from "../../style/variables.style";
 
-export const Elem = styled.div``;
-
-export const WeatherWrapper = styled.div`
+export const WrapperWeather = styled.div`
   font-family: ${GFonts.inter};
   display: flex;
   flex-direction: column;
