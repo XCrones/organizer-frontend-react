66405363e1fc65e819526f3179ad58f3647b07c4
XCrone <ls.xcrone@gmail.com>
2023-03-10 11:34:25 +0300

    replace: type to style

--- src/components/calendar/Month.component.tsx
+++ src/components/calendar/Month.component.tsx
@@ -10,11 +10,11 @@ interface Props {
 const MonthComponent = ({ monthStr, nextMonth, prevMonth, yearNum }: Props) => {
   return (
     <Moth>
-      <MonthPrev onClick={() => prevMonth()} type="button">
+      <MonthPrev onClick={() => prevMonth()}>
         <i className="bi bi-chevron-left"></i>
       </MonthPrev>
       <MonthCurr>{`${monthStr} ${yearNum}`}</MonthCurr>
-      <MonthNext onClick={() => nextMonth()} type="button">
+      <MonthNext onClick={() => nextMonth()}>
         <i className="bi bi-chevron-right"></i>
       </MonthNext>
     </Moth>
