edd92a9183044d9ee0aea77b2cb9447fca5f6335
XCrone <ls.xcrone@gmail.com>
2023-03-10 13:05:32 +0300

    join: WEATHER_CONFIG

--- src/config/components/components-config.ts
+++ src/config/components/components-config.ts
@@ -4,3 +4,11 @@ export const TODO_CONFIG = {
     rounded: 15,
   },
 };
+
+export const WEATHER_CONFIG = {
+  forecast_list: {
+    overscanCount: 2,
+    height: 190,
+    itemSize: 110,
+  },
+};
