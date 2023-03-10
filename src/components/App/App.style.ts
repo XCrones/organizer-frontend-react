ccad08090fc7ccf0c218e65f66870a9fb18de216
XCrone <ls.xcrone@gmail.com>
2023-03-10 17:10:25 +0300

    ref: delete interface, add: theme style

--- src/components/App/App.style.ts
+++ src/components/App/App.style.ts
@@ -1,21 +1,17 @@
 import styled from "styled-components";
 
-interface IWrapper {
-  colorBg: string;
-  height_footer: number;
-}
-
-export const Wrapper = styled.div<IWrapper>`
+export const Wrapper = styled.div<{ height_footer: number }>`
   min-height: 100vh;
   min-width: 100%;
-  color: #fff;
-  background-color: ${(props) => props.colorBg};
-
   display: grid;
   grid-template-columns: 1fr;
   grid-template-rows: 1fr ${(props) => props.height_footer}px;
   grid-column-gap: 0px;
   grid-row-gap: 0px;
+
+  color: ${(props) => props.theme.color.main};
+  background-color: ${(props) => props.theme.background.main};
+  transition: background-color 0.3s ease-in-out;
 `;
 
 export const Section = styled.section<{ maxHeight: number; paddingBottom: number }>`
