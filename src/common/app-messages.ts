59abadb2e66ee5d0588c2fd8508ffb928e3800fa
XCrone <ls.xcrone@gmail.com>
2023-03-10 11:03:32 +0300

    add: new REQ_FIELD, EMAIL_INV, MIN_CHAR, MAX_CHAR

--- src/common/app-messages.ts
+++ src/common/app-messages.ts
@@ -2,4 +2,7 @@ export const APP_MESSAGES = {
   succes: (text?: string | string[]) => `Succes:${!!text ? ` ${text}` : ""}!`,
   error: (text?: string | string[]) => `Error:${!!text ? ` ${text}` : ""}!`,
   warn: (text?: string | string[]) => `Warning:${!!text ? ` ${text}` : ""}!`,
+  REQ_FIELD: "Required filed",
+  EMAIL_INV: "Email invalid",
+  MIN_CHAR: (count: number) => `Min chars ${String(count)}`,
 };
