7fc66b9c6f27272058222d296520865025ff81ee
XCrone <ls.xcrone@gmail.com>
2023-03-10 17:11:21 +0300

    add: typeTheme

--- src/store/settings.store.ts
+++ src/store/settings.store.ts
@@ -1,18 +1,21 @@
 import { create } from "zustand";
 import { persist, createJSONStorage } from "zustand/middleware";
+import { DefaultTheme } from "styled-components";
+import { DarkTheme, LightTheme } from "../ui/theme";
 
 interface SettingsStore {
+  typeTheme: DefaultTheme;
   isNightTheme: boolean;
   toggleTheme: () => void;
 }
 export const useSettingsStore = create<SettingsStore>()(
   persist(
     (set, get) => ({
+      typeTheme: DarkTheme,
       isNightTheme: true,
-      toggleTheme: () =>
-        set({
-          isNightTheme: !get().isNightTheme,
-        }),
+      toggleTheme: () => {
+        set({ isNightTheme: !get().isNightTheme, typeTheme: !get().isNightTheme ? DarkTheme : LightTheme });
+      },
     }),
     {
       name: "settings",
