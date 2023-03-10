5d38794fa09fa610042a4539c3a1c7e9e06232f3
XCrone <ls.xcrone@gmail.com>
2023-03-10 11:38:52 +0300

    ref: rename components

--- src/components/header/Header.component.tsx
+++ src/components/header/Header.component.tsx
@@ -1,5 +1,5 @@
 import { IHeaderButton } from "../../models";
-import { HeaderButt, HeaderButtns, HeaderTitle, HeaderWrapper } from "./Header.style";
+import { HeaderButt, HeaderButtns, HeaderTitle, Header } from "./Header.style";
 
 interface IProps {
   title: string;
@@ -8,14 +8,14 @@ interface IProps {
 
 const HeaderComponent = ({ title, buttns }: IProps) => {
   return (
-    <HeaderWrapper>
+    <Header>
       <HeaderTitle>{title}</HeaderTitle>
       <HeaderButtns>
         {buttns.map((butt, idx) => (
-          <HeaderButt key={idx} onClick={() => butt.callback()} type="button" className={butt.icon} />
+          <HeaderButt key={idx} onClick={() => butt.callback()} className={butt.icon} />
         ))}
       </HeaderButtns>
-    </HeaderWrapper>
+    </Header>
   );
 };
 
