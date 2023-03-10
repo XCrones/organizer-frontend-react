a876f0304100acae707744c2d3581606e7f8418f
XCrone <ls.xcrone@gmail.com>
2023-03-10 12:45:56 +0300

    join: triangle

---
+++ src/ui/components/triangle.style.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { ITriangle } from "../../models";
+
+// prettier-ignore
+export const GTriangle = styled.div<ITriangle>`
+  width: 0;
+  height: 0;
+  border-style: solid;
+  border-width: ${(props) => props.size.t}px ${(props) => props.size.r}px ${(props) => props.size.b}px
+    ${(props) => props.size.l}px;
+  border-color: ${(props) => props.borderColor.t} ${(props) => props.borderColor.r} ${(props) => props.borderColor.b} ${(props) => props.borderColor.l};
+`;
