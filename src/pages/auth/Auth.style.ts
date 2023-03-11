32e8abcc19acb07b09373c8be2b2a5a32d041ec0
XCrone <ls.xcrone@gmail.com>
2023-03-11 11:13:58 +0300

    add: G_VARIABLES & padding

--- src/pages/auth/Auth.style.ts
+++ src/pages/auth/Auth.style.ts
@@ -1,9 +1,10 @@
 import styled from "styled-components";
+import { G_VARIABLES } from "../../ui/variables";
 
-export const AuthWrapper = styled.div<{ pr: number; pl: number }>`
+export const AuthWrapper = styled.div`
   height: 100%;
   width: 100%;
   padding-top: 40px;
-  padding-left: ${(props) => props.pl}px;
-  padding-right: ${(props) => props.pr}px;
+  padding-left: ${G_VARIABLES.indent.left}px;
+  padding-right: ${G_VARIABLES.indent.right}px;
 `;
