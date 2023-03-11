ae5675e507240a1de18c1dad08d5b6fa8f98d2da
XCrone <ls.xcrone@gmail.com>
2023-03-11 12:49:45 +0300

    rm: html_symbol, join: IG_VARIABLES

--- src/ui/variables.tsx
+++ src/ui/variables.tsx
@@ -1,4 +1,80 @@
-export const G_VARIABLES = {
+interface G_VARIABLES {
+  transition: {
+    theme: `all 300ms ease-in-out`;
+  };
+  backgrund: {
+    section: {
+      dark: "#171930";
+      light: "#d7dee4";
+    };
+    footer: {
+      dark: "#0D0F22";
+      light: "#FFFFFF";
+    };
+    nav: {
+      dark: {
+        default: "#63636d";
+        select: "#fff";
+      };
+      light: {
+        default: "#878792";
+        select: "#000";
+      };
+    };
+    dark_blue: "#1E203B";
+    medium_blue: "#383b5f";
+  };
+  color: {
+    section: {
+      dark: "#fff";
+      light: "#000";
+    };
+    footer: {
+      dark: "#fff";
+      light: "#000";
+    };
+    priority: {
+      hight: "#ff0000";
+      medium: "#ff8800";
+      low: "#00ff00";
+    };
+    link: {
+      dark: "#0D0F22";
+      light: "#707070";
+    };
+    error: {
+      red: "#ff0000";
+      orange: "#ff7b00";
+    };
+    pallete: [
+      "#0066ff",
+      "#6666ff",
+      "#ff33cc",
+      "#ff0066",
+      "#ff6600",
+      "#ccff33",
+      "#cc33ff",
+      "#66ff33",
+      "#00ff00",
+      "#00ffcc",
+      "#ff3300",
+      "#ffcc00"
+    ];
+    light_gray: "#707070";
+    gradient: {
+      red: ["#871111", "#e03138"];
+      blue: ["#266ED7", "#4D8AEB"];
+    };
+  };
+  indent: {
+    left: 20;
+    right: 20;
+    top: 0;
+    bottom: 45;
+  };
+}
+
+export const G_VARIABLES: G_VARIABLES = {
   transition: {
     theme: `all 300ms ease-in-out`,
   },
@@ -71,9 +147,5 @@ export const G_VARIABLES = {
     right: 20,
     top: 0,
     bottom: 45,
-  },
-  html_symbol: {
-    deg: <>&deg;</>,
-    nbsp: <>&deg;</>,
   },
 };
