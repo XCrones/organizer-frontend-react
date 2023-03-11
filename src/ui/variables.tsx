8f448bee0fe0fda84c1c49b391e4e449e3ccdf77
XCrone <ls.xcrone@gmail.com>
2023-03-11 11:17:20 +0300

    rm: *.styles

---
+++ src/ui/variables.tsx
@@ -0,0 +1,69 @@
+export const G_VARIABLES = {
+  transition: {
+    theme: `all 300ms ease-in-out`,
+  },
+  backgrund: {
+    section: {
+      dark: "#171930",
+      light: "#F8F8FA",
+    },
+    footer: {
+      dark: "#0D0F22",
+      light: "#bfbfcc",
+    },
+    dark_blue: "#1E203B",
+    medium_blue: "#383b5f",
+  },
+  color: {
+    section: {
+      dark: "#fff",
+      light: "#000",
+    },
+    footer: {
+      dark: "#fff",
+      light: "#000",
+    },
+    priority: {
+      hight: "#ff0000",
+      medium: "#ff8800",
+      low: "#00ff00",
+    },
+    link: {
+      dark: "#0D0F22",
+      light: "#707070",
+    },
+    error: {
+      red: "#ff0000",
+      orange: "#ff7b00",
+    },
+    pallete: [
+      "#0066ff",
+      "#6666ff",
+      "#ff33cc",
+      "#ff0066",
+      "#ff6600",
+      "#ccff33",
+      "#cc33ff",
+      "#66ff33",
+      "#00ff00",
+      "#00ffcc",
+      "#ff3300",
+      "#ffcc00",
+    ],
+    light_gray: "#707070",
+    gradient: {
+      red: ["#871111", "#e03138"],
+      blue: ["#266ED7", "#4D8AEB"],
+    },
+  },
+  indent: {
+    left: 20,
+    right: 20,
+    top: 0,
+    bottom: 45,
+  },
+  html_symbol: {
+    deg: <>&deg;</>,
+    nbsp: <>&deg;</>,
+  },
+};
