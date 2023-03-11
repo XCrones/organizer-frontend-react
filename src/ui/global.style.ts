c665baf95c23b3eb1950e117425327eec912fc84
XCrone <ls.xcrone@gmail.com>
2023-03-11 11:16:23 +0300

    join: global style

--- src/ui/global.style.ts
+++ src/ui/global.style.ts
@@ -1,3 +1,20 @@
 import { createGlobalStyle } from "styled-components";
 
-export default createGlobalStyle``;
+export default createGlobalStyle`
+  * {
+      &::-webkit-scrollbar {
+      width: 4px;
+      height: 0px;
+      background-color: #143861;
+    }
+
+    &::-webkit-scrollbar-thumb {
+      background-color: #347184;
+      border-radius: 10px;
+
+      &:hover {
+        background-color: #7ed6cf;
+      }
+    }
+  }
+`;
