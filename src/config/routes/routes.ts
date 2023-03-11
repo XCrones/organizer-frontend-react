16e24a91a4bb530f80eb79adbb99a9e85e8578cf
XCrone <ls.xcrone@gmail.com>
2023-03-11 12:18:47 +0300

    replace: items nav to types

--- src/config/routes/routes.ts
+++ src/config/routes/routes.ts
@@ -1,15 +1,45 @@
-export interface IRouterLinkItem {
-  PATH: string;
-  TITLE: string;
-}
+export type TCalendarTitle = "calendar";
+export type TCalendarPath = "/calendar";
 
+export type TTodoTitle = "todos";
+export type TTodoPath = "/todos";
+
+export type TWeatherTitle = "weather";
+export type TWeatherPath = "/weather";
+
+export type TAuthTitle = "auth";
+export type TAuthPath = "/auth";
+
+export type TSettingsTitle = "settings";
+export type TSettingsPath = "/settings";
+
+export type THomeTitle = "home";
+export type THomePath = "/";
 interface IRouterLinks {
-  CALENDAR: IRouterLinkItem;
-  TODOS: IRouterLinkItem;
-  WEATHER: IRouterLinkItem;
-  SETTINGS: IRouterLinkItem;
-  AUTH: IRouterLinkItem;
-  HOME: IRouterLinkItem;
+  CALENDAR: {
+    PATH: TCalendarPath;
+    TITLE: TCalendarTitle;
+  };
+  TODOS: {
+    PATH: TTodoPath;
+    TITLE: TTodoTitle;
+  };
+  WEATHER: {
+    PATH: TWeatherPath;
+    TITLE: TWeatherTitle;
+  };
+  SETTINGS: {
+    PATH: TSettingsPath;
+    TITLE: TSettingsTitle;
+  };
+  AUTH: {
+    PATH: TAuthPath;
+    TITLE: TAuthTitle;
+  };
+  HOME: {
+    PATH: THomePath;
+    TITLE: THomeTitle;
+  };
 }
 
 export const ROUTES: IRouterLinks = {
