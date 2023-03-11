16d061e9fbe0446b5a5b7337a598b2f97f3f2665
XCrone <ls.xcrone@gmail.com>
2023-03-11 11:13:38 +0300

    move: padding to styles

--- src/pages/auth/Auth.page.tsx
+++ src/pages/auth/Auth.page.tsx
@@ -1,13 +1,12 @@
 import { useState } from "react";
 import { AuthSignInComponent, AuthSignUpComponent } from "../../components";
-import { G_INDENTS } from "../../ui/variables.style";
 import { AuthWrapper } from "./Auth.style";
 
 const AuthPage = () => {
   const [isSignIn, toggleForm] = useState(true);
 
   return (
-    <AuthWrapper pl={G_INDENTS.left} pr={G_INDENTS.right}>
+    <AuthWrapper>
       {isSignIn && <AuthSignInComponent toggleForm={() => toggleForm(!isSignIn)} />}
       {!isSignIn && <AuthSignUpComponent toggleForm={() => toggleForm(!isSignIn)} />}
     </AuthWrapper>
