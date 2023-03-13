b2a0cf7167d5f290c7907fdf048d6d05dbb3574a
XCrone <ls.xcrone@gmail.com>
2023-03-13 16:31:16 +0300

    add: new fields

--- src/models/theme.model.ts
+++ src/models/theme.model.ts
@@ -18,9 +18,119 @@ export interface ITheme {
       };
     };
     settings: {
+      title: string;
       btn: {
         submit: {
           gradient: string[];
+          color: string;
+        };
+      };
+    };
+    weather: {
+      cities: {
+        item: {
+          background: string;
+          color: string;
+        };
+        btn: {
+          color: string;
+        };
+      };
+      forecast: {
+        background: {
+          item: string;
+        };
+        color: {
+          item: string;
+        };
+        iconWeather: {
+          background: string;
+        };
+        day_times: {
+          background: {
+            select: string;
+            default: string;
+          };
+          color: {
+            select: string;
+            default: string;
+          };
+        };
+        descr: {
+          background: string;
+          color: string;
+        };
+      };
+      join: {
+        background: string;
+        color: string;
+        field: {
+          background: string;
+          color: string;
+        };
+        icon_color: string;
+        submit: {
+          gradient: string[];
+          color: string;
+        };
+      };
+    };
+    calendar: {
+      month: {
+        btn: {
+          color: string;
+        };
+      };
+      day: {
+        gradient: string[];
+        border: {
+          select: string;
+          default: string;
+        };
+      };
+    };
+    todos: {
+      background: string;
+      color: {
+        title: string;
+        subtitle: string;
+      };
+      chekcbox: {
+        active: string;
+      };
+    };
+    auth: {
+      submit: {
+        gradient: string[];
+        color: string;
+      };
+      title: string;
+      subtitle: string;
+      field: {
+        background: string;
+        label: string;
+        text_color: string;
+        error: string;
+      };
+    };
+    editor: {
+      background: string;
+      color: string;
+      field: {
+        background: string;
+        color: string;
+      };
+      butt: {
+        delete: {
+          graient: string[];
+          color: string;
+        };
+        submit: {
+          graient: string[];
+          color: string;
+        };
+        cancel: {
+          graient: string[];
           color: string;
         };
       };
