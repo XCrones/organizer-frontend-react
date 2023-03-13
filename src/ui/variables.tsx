4f3860304450f4d127fe5d43e9a596b9ed4505cc
XCrone <ls.xcrone@gmail.com>
2023-03-13 16:31:55 +0300

    refactor

--- src/ui/variables.tsx
+++ src/ui/variables.tsx
@@ -1,160 +1,15 @@
-interface IG_VARIABLES {
-  transition: {
-    theme: `all 300ms ease-in-out`;
-  };
-  backgrund: {
-    section: {
-      dark: "#171930";
-      light: "#F6F7F9";
-    };
-    footer: {
-      dark: "#0D0F22";
-      light: "#d7dee4";
-    };
-    nav: {
-      dark: {
-        default: "#63636d";
-        select: "#fff";
-      };
-      light: {
-        default: "#878792";
-        select: "#000";
-      };
-    };
-    dark_blue: "#1E203B";
-    medium_blue: "#383b5f";
-    block: {
-      dark: "#383b5f";
-      light: "#c8c8dd";
-    };
-  };
-  color: {
-    section: {
-      dark: "#fff";
-      light: "#000";
-    };
-    footer: {
-      dark: "#fff";
-      light: "#000";
-    };
-    priority: {
-      hight: "#ff0000";
-      medium: "#ff8800";
-      low: "#00ff00";
-    };
-    link: {
-      dark: "#0D0F22";
-      light: "#707070";
-    };
-    error: {
-      red: "#ff0000";
-      orange: "#ff7b00";
-    };
-    pallete: [
-      "#0066ff",
-      "#6666ff",
-      "#ff33cc",
-      "#ff0066",
-      "#ff6600",
-      "#ccff33",
-      "#cc33ff",
-      "#66ff33",
-      "#00ff00",
-      "#00ffcc",
-      "#ff3300",
-      "#ffcc00"
-    ];
-    light_gray: "#707070";
-    gradient: {
-      red: {
-        dark: ["#871111", "#e03138"];
-        lightight: ["#871111", "#e03138"];
-      };
-      blue: {
-        dark: ["#266ED7", "#4D8AEB"];
-        light: ["#2160be", "#4073be"];
-      };
-    };
-    block: {
-      title: {
-        dark: "#fff";
-        light: "#000";
-      };
-      subtitle: {
-        dark: "#a59d9d";
-        light: "#292626";
-      };
-    };
-    white: "#fff";
-    black: "#000";
-  };
-  indent: {
-    left: 20;
-    right: 20;
-    top: 0;
-    bottom: 45;
-  };
-}
-
-export const G_VARIABLES: IG_VARIABLES = {
+export const G_VARIABLES = {
   transition: {
     theme: `all 300ms ease-in-out`,
   },
-  backgrund: {
-    section: {
-      dark: "#171930",
-      light: "#F6F7F9",
-    },
-    footer: {
-      dark: "#0D0F22",
-      light: "#d7dee4",
-    },
-    nav: {
-      dark: {
-        default: "#63636d",
-        select: "#fff",
-      },
-      light: {
-        default: "#878792",
-        select: "#000",
-      },
-    },
-    dark_blue: "#1E203B",
-    medium_blue: "#383b5f",
-    block: {
-      dark: "#383b5f",
-      light: "#c8c8dd",
-    },
-  },
-  color: {
-    section: {
-      dark: "#fff",
-      light: "#000",
-    },
-    footer: {
-      dark: "#fff",
-      light: "#000",
-    },
-    priority: {
-      hight: "#ff0000",
-      medium: "#ff8800",
-      low: "#00ff00",
-    },
-    link: {
-      dark: "#0D0F22",
-      light: "#707070",
-    },
-    error: {
-      red: "#ff0000",
-      orange: "#ff7b00",
-    },
-    pallete: [
+  pallete: {
+    editor: [
       "#0066ff",
       "#6666ff",
       "#ff33cc",
       "#ff0066",
       "#ff6600",
-      "#ccff33",
+      "#11ca11",
       "#cc33ff",
       "#66ff33",
       "#00ff00",
@@ -162,29 +17,81 @@ export const G_VARIABLES: IG_VARIABLES = {
       "#ff3300",
       "#ffcc00",
     ],
-    light_gray: "#707070",
-    gradient: {
-      red: {
-        dark: ["#871111", "#e03138"],
-        lightight: ["#871111", "#e03138"],
-      },
-      blue: {
-        dark: ["#266ED7", "#4D8AEB"],
-        light: ["#2160be", "#4073be"],
-      },
+  },
+  gradient: {
+    red: {
+      medium: ["#871111", "#e03138"],
+      light: ["#a12424", "#e45459"],
     },
-    block: {
-      title: {
-        dark: "#fff",
-        light: "#000",
-      },
-      subtitle: {
-        dark: "#a59d9d",
-        light: "#292626",
-      },
+    blue: {
+      medium: ["#266ED7", "#4D8AEB"],
+      light: ["#2160be", "#4073be"],
     },
+    purple_orange: {
+      medium: ["#5B34FF", "#FF5303"],
+      light: ["#3e23a8", "#b33901"],
+    },
+    purple_pink: {
+      medium: ["#654ea3", "#eaafc8"],
+      light: ["#5231aa", "#ec83af"],
+    },
+    teal: {
+      light: ["#259e80", "#21b3b3"],
+      medium: ["#339179", "#2b9292"],
+    },
+  },
+  color: {
     white: "#fff",
     black: "#000",
+    gray: {
+      light: "#707070",
+      medium: "#414747",
+      hight: "#313333",
+    },
+    blue_light: {
+      light: "#7a92c5",
+      medium: "#5274bd",
+      hight: "#476cbb",
+    },
+    blue: {
+      light: "#4d8cca",
+      medium: "#16599b",
+      hight: "#102e4b",
+    },
+    green: {
+      light: "#00ff0d",
+      medium: "#0fb918",
+      hight: "#0a740f",
+    },
+    orange: {
+      light: "#f16934",
+      medium: "#d85724",
+      hight: "#ca410b",
+    },
+    red: {
+      hight: "#ff0000",
+      medium: "#d83434",
+      light: "#f06262",
+    },
+    dark_blue: {
+      light: "#383b5f",
+      medium: "#171930",
+      hight: "#0D0F22",
+    },
+    light_blue: {
+      light: "#b6b6dd",
+    },
+    light_gray: {
+      light: "#F6F7F9",
+      medium: "#d7dee4",
+      hight: "#878792",
+    },
+    light_black: {
+      light: "#292626",
+    },
+    purple: {
+      light: "#200f4e",
+    },
   },
   indent: {
     left: 20,
