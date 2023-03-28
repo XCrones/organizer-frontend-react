30ee8d2323aee2791c0d29d73a25b656f6b77e3b
Serjey <ls.xcrone@gmail.com>
2023-03-28 12:45:15 +0300

    join: enums

---
+++ src/models/Enum/ERoutes.ts
@@ -0,0 +1,17 @@
+export enum ERoutesPath {
+  HOME = "/",
+  TODO = "/todo",
+  WEATHER = "/weather",
+  CALENDAR = "/calendar",
+  SETTINGS = "/settings",
+  AUTH = "/auth",
+}
+
+export enum ERoutesTitle {
+  HOME = "home",
+  TODO = "todo",
+  WEATHER = "weather",
+  CALENDAR = "calendar",
+  SETTINGS = "settings",
+  AUTH = "auth",
+}
